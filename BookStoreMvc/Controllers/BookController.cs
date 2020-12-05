7dc9fad8fc64bcdc430a670c9e3fdd4caa599dc0
simon441 <simon.cateau@yahoo.fr>
2020-12-06 00:20:36 +0100

    Add new Book Controller

---
+++ BookStoreMvc/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreMvc.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetAllBooks()
+        {
+            return "All Books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"Book with id = {id}";
+        }
+
+        public string SearchBooks(string bookName, string authorName)
+        {
+            return $"Book with name: {bookName} and author: {authorName}";
+        }
+    }
+}

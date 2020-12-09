ef23af4fc058ec341948d6c564d45446c25c183a
simon441 <simon.cateau@yahoo.fr>
2020-12-09 20:33:54 +0100

    Part #58: add optgroup with SelectListGroup to the dropdown

--- BookStoreMvc/Controllers/BookController.cs
+++ BookStoreMvc/Controllers/BookController.cs
@@ -54,11 +54,18 @@ namespace BookStoreMvc.Controllers
             //    Value = x.Id.ToString(),
             //}).ToList();
 
+            var group1 = new SelectListGroup() { Name = "Group 1" };
+            var group2 = new SelectListGroup() { Name = "Group 2", Disabled=true };
+            var group3 = new SelectListGroup() { Name = "Group 3" };
+
             ViewBag.Language = new List<SelectListItem>() {
-                new SelectListItem(){Text="English", Value="1" },
-                new SelectListItem(){Text="French", Value="2", Selected=true },
-                new SelectListItem(){Text="Spanish", Value="3" , Disabled=true},
-                new SelectListItem(){Text="Chinese", Value="4", Disabled=true },
+                new SelectListItem(){Text="English", Value="1", Group=group1 },
+                new SelectListItem(){Text="French", Value="2",Group=group1 },
+                new SelectListItem(){Text="Spanish", Value="3" , Group=group1 },
+                new SelectListItem(){Text="Chinese", Value="4", Group=group2 },
+                new SelectListItem(){Text="Dutch", Value="5", Group=group3 },
+                new SelectListItem(){Text="Japanese", Value="6",  Group=group2},
+                new SelectListItem(){Text="Greek", Value="7",  Group=group3},
             };
 
             ViewBag.IsSuccess = isSuccess;

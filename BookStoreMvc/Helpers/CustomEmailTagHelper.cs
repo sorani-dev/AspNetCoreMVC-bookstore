4098e47a5515e2deb59012ab4ad5df403b650513
simon441 <simon.cateau@yahoo.fr>
2020-12-10 00:52:03 +0100

    Part #63: Added a custom email tag helper

---
+++ BookStoreMvc/Helpers/CustomEmailTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreMvc.Helpers
+{
+    public class CustomEmailTagHelper : TagHelper
+    {
+        public string MyEmail { get; set; }
+
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", $"mailto:{MyEmail}");
+            output.Attributes.Add("id", "my-email-id");
+            output.Content.SetContent("my-email");
+            return base.ProcessAsync(context, output);
+        }
+    }
+}

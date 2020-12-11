c23d381a24597d12308efb6d67d3b70bcb215a8a
simon441 <simon.cateau@yahoo.fr>
2020-12-11 22:11:45 +0100

    Part #92: signup form design with model

---
+++ BookStoreMvc/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using BookStoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreMvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("signup")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [Route("signup")]
+        [HttpPost]
+        public IActionResult SignUp(SignUpUserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}

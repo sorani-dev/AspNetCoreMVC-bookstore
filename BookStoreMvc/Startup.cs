bd101920ea048af3351855d38c2773494818573b
simon441 <simon.cateau@yahoo.fr>
2020-12-11 18:34:13 +0100

    Part #82: Read connection string from app settings.jsoon

--- BookStoreMvc/Startup.cs
+++ BookStoreMvc/Startup.cs
@@ -3,6 +3,7 @@ using BookStoreMvc.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,12 +11,20 @@ namespace BookStoreMvc
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<BookStoreContext>(
-                options => options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Database = BookStore; Integrated Security = true; "));
+                options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+                );
             services.AddControllersWithViews();
 
 #if DEBUG            

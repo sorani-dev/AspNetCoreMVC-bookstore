fc6715557da18518d83aa72355fb1224e43dd786
simon441 <simon.cateau@yahoo.fr>
2020-12-12 05:33:09 +0100

    Part #104: Create custom SMTP email service

---
+++ BookStoreMvc/Services/EmailService.cs
@@ -0,0 +1,63 @@
+﻿using BookStoreMvc.Models;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreMvc.Services
+{
+    public class EmailService
+    {
+        private const string templatePath = @"EmailTemplate/{0}.html";
+        private readonly SMTPConfigModel smtpConfig;
+
+        public EmailService(IOptions<SMTPConfigModel> options)
+        {
+            this.smtpConfig = options.Value;
+        }
+
+        private async Task SendEmail(UserEmailOptions userEmailOptions)
+        {
+            MailMessage mail = new MailMessage()
+            {
+                Body = userEmailOptions.Body,
+                Subject = userEmailOptions.Subject,
+                From = new MailAddress(smtpConfig.SenderAddress, smtpConfig.SenderDisplayName),
+                IsBodyHtml = smtpConfig.IsBodyHTML,
+            };
+
+            foreach (var toEmail in userEmailOptions.ToEmails)
+            {
+                mail.To.Add(toEmail);
+            }
+
+            NetworkCredential networkCredential = new NetworkCredential(smtpConfig.UserName, smtpConfig.Password);
+
+            SmtpClient smtpClient = new SmtpClient
+            {
+                Host = smtpConfig.Host,
+                Port = smtpConfig.Port,
+                EnableSsl = smtpConfig.EnableSSL,
+                UseDefaultCredentials = smtpConfig.UseDefaultCredentials,
+                Credentials = networkCredential
+            };
+
+            mail.BodyEncoding = Encoding.Default;
+
+
+            await smtpClient.SendMailAsync(mail);
+
+        }
+
+        public string GetEmailBody(string templateName)
+        {
+            var body = File.ReadAllText(String.Format(templatePath, templateName));
+            return body;
+        }
+    }
+}
